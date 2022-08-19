ccb223c209a4ac490e274231165016336252f357
beetz1985 <benjamintoulson@hotmail.co.uk>
2022-08-19 23:07:17 +0100

    Added state as an object, changed object/state value

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,12 +3,38 @@ import data from '../memesData'
 
 function Meme() {
 
-    const [newImageState, setState] = React.useState('');
+    // const [newImageState, setState] = React.useState('');
     
+    // function getMeme(e) {
+    //     e.preventDefault();
+    //     const randomIndex = Math.floor(Math.random() * data.data.memes.length);
+    //     setState(data.data.memes[randomIndex].url);
+    // }
+
+
+    const [state_one, setState_one] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    })
+
+    const [state_two, setState_two] = React.useState(data);
+
     function getMeme(e) {
         e.preventDefault();
-        const randomIndex = Math.floor(Math.random() * data.data.memes.length);
-        setState(data.data.memes[randomIndex].url);
+
+        const memesArray = state_two.data.memes;
+        const randomIndex = Math.floor(Math.random() * memesArray.length);
+
+        setState_one((s)=>{
+            return {
+                ...s,
+                randomImage: memesArray[randomIndex].url
+            }
+        })
+        
+
+
     }
 
     return (
@@ -18,9 +44,11 @@ function Meme() {
                 <input className="meme-end" type="text" placeholder="Bottom Text"/>
                 <button onClick={getMeme}>Get New Meme Image &#128444;&#65039;</button>
             </form>
-
-            {(newImageState) ? <img className="meme-image" src={newImageState} alt="img"/> : ''}
-            
+            <div className="image-container">
+                <h2>{state_one.topText}</h2>
+                {(state_one.randomImage) ? <img className="meme-image" src={state_one.randomImage} alt="img"/> : ''}
+                <h2>{state_one.bottomText}</h2>
+            </div>
         </main>
     )
 }
